5e908df8639fc291d55d9d45faef7998b22e2b87
Abhishek Jee <abhishek.jee.2411@gmail.com>
2017-12-24 17:45:38 +0530

    Modifying list_less_than_10.py

--- list_less_than_10.py
+++ list_less_than_10.py
@@ -12,3 +12,20 @@ i = 0
 while (i < len(a)):
 	if a[i] < 5: print (a[i]," is less than 5")
 	i += 1
+
+print ("Transferring all the elements less than 5 into a new list")
+i = 0
+j = 0
+b = [] 						#creating an empty list named "b"
+while (i < len(a)):
+	if a[i] < 5:
+		b.append(a[i])
+		j += 1
+	i += 1
+
+print ("Printing the new list")
+i=0
+print ("The length of the new list is : ",len(b))
+while (i < len(b)):
+	print (b[i])
+	i += 1
